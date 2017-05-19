d30656541f46e1cc6e1d62c5a3bc961d7d5f7633
gt-java <galinaTim0417@gmail.com>
2017-05-19 00:45:54 -0700

    initial commit SA

---
+++ src/main/java/core/CheckStyle.java
@@ -0,0 +1,10 @@
+package core;
+// http://checkstyle.sourceforge.net/
+public class CheckStyle {
+
+	// LineLengthCheck
+	void MethodNameMethodNameMethodName() {
+		System.out.println("Schoolmaster amnd The classroom is anagram? - true");
+	}
+
+}
